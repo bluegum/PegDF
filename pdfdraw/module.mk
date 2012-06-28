d9481babc0b3837064f596a4272f8332e0124509
Dakai Liu <dakailiu.os@gmail.com>
2012-06-28 21:47:13 +1000

    Update README

--- pdfdraw/module.mk
+++ pdfdraw/module.mk
@@ -6,7 +6,9 @@ SRCS_$(d)	:= $(d)/pdfdraw.c
 
 OBJS_$(d)	:= $(SRCS_$(d):%.c=%.o)
 
-DEPS_$(d)	:= $(OBJS_$(d):%=%.d)
+DEPS_$(d)	:= $(SRCS_$(d):%.c=%.d)
+
+include 	$(DEPS_$(d))
 
 CLEAN		:= $(CLEAN) $(OBJS_$(d)) $(DEPS_$(d))
 
