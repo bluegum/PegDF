a1ebc0a7ebb2e96f4009fab721ef71e7849f6acc
Dakai Liu <dakailiu.os@gmail.com>
2012-07-29 12:40:50 +1000

    add missed pdfread sub-directory's make module

---
+++ pdfread/module.mk
@@ -0,0 +1,23 @@
+d               := pdfread
+
+LOCAL_LIB       := $(d)/libpdfread.a
+
+SRCS__$(d)      := pdfread.c
+
+SRCS_$(d)       := $(addprefix $(d)/, $(SRCS__$(d)))
+
+OBJS_$(d)       := $(SRCS_$(d):%.c=%.o)
+
+DEPS_$(d)       := $(SRCS_$(d):%.c=%.d)
+
+include         $(DEPS_$(d))
+
+HDRS_$(d)       := $(wildcard $(d)/*.h)
+
+CLEAN           := $(CLEAN) $(OBJS_$(d)) $(DEPS_$(d))
+
+TGT_LIB         := $(TGT_LIB) $(LOCAL_LIB)
+
+$(LOCAL_LIB) : $(OBJS_$(d))
+	@echo $^ 
+	$(ARCHIVE)
